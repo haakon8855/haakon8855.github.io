b76d9112a8bd6d0da52b608f563e3b0f86afee0e
Håkon Anders <haakon8855@users.noreply.github.com>
2022-08-16 00:05:34 +0200

    Add navbar highlighting
    Highlight current page in navbar by making links to other pages darker
    color than link to current page.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import "../styles/Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation().pathname;
+
   document.documentElement.dataset.scroll = "0";
   document.addEventListener("scroll", () => {
     document.documentElement.dataset.scroll = "" + window.scrollY;
@@ -10,9 +12,15 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="links">
-        <Link to="/">Character</Link>
-        <Link to="/quests">Quests</Link>
-        <Link to="/log">Log</Link>
+        <Link to="/">
+          <div className={location == "/" ? "current" : ""}>Character</div>
+        </Link>
+        <Link to="/quests">
+          <div className={location == "/quests" ? "current" : ""}>Quests</div>
+        </Link>
+        <Link to="/log">
+          <div className={location == "/log" ? "current" : ""}>Log</div>
+        </Link>
       </div>
     </div>
   );
