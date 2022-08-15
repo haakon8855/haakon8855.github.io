e0ddb91c0d5a5b59220514718c176853fe1fc611
Håkon Anders <haakon8855@users.noreply.github.com>
2022-08-15 23:46:55 +0200

    Fix scrolling on reload
    Remove css to fix scrolling on reload
    Remove unused code

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,11 @@
 import "../styles/Navbar.css";
-import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useLocation } from "react-router";
 
 const Navbar = () => {
-  history.scrollRestoration = "manual";
-  // window.scrollTo(0, 0);
   document.documentElement.dataset.scroll = "0";
   document.addEventListener("scroll", () => {
     document.documentElement.dataset.scroll = "" + window.scrollY;
   });
-
-  // const location = useLocation();
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, [location]);
 
   return (
     <div className="navbar">
