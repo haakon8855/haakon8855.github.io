6cbb498225e28e586e2d7d184f6d046b58339b92
Håkon <haakon8855@users.noreply.github.com>
2022-08-25 20:37:41 +0200

    Add scroll button on homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,29 @@
 import "../styles/Home.css";
 import Character from "./Character";
+import { scroller, Element } from "react-scroll";
 
 const Home = () => {
+  const scrollToInventory = () => {
+    scroller.scrollTo("character-info", {
+      duration: 500,
+      offset: -58,
+      smooth: true,
+    });
+  };
+
   return (
     <div>
       <div className="home">
         <div className="greeting">
           <h1>Hello there!</h1>
           <h1>I'm Håkon</h1>
+          <button className="character-info-button" onClick={scrollToInventory}>
+            <div className="character-info-button-text">Character Info</div>
+          </button>
         </div>
-        <Character />
+        <Element name="character-info">
+          <Character />
+        </Element>
       </div>
     </div>
   );
