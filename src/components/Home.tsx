930807e4b98b391c1955f585d8d9779e5ce69aca
Håkon <haakon8855@users.noreply.github.com>
2022-08-25 21:32:16 +0200

    Add information text to homepage
    Add text under name on homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,20 @@
 import "../styles/Home.css";
 import Character from "./Character";
 import { scroller, Element } from "react-scroll";
+import { useEffect, useState } from "react";
 
 const Home = () => {
+  const [isSmall, setIsSmall] = useState(window.innerWidth >= 500);
+
+  const updateIsSmall = () => {
+    setIsSmall(window.innerWidth >= 500);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", updateIsSmall);
+    return () => window.removeEventListener("resize", updateIsSmall);
+  });
+
   const scrollToInventory = () => {
     scroller.scrollTo("character-info", {
       duration: 500,
@@ -17,6 +29,10 @@ const Home = () => {
         <div className="greeting">
           <h1>Hello there!</h1>
           <h1>I'm Håkon</h1>
+          <h3>
+            I'm a Computer Science masters student {isSmall ? <br /> : ""}
+            specialising in Artficial Intelligence
+          </h3>
           <button className="character-info-button" onClick={scrollToInventory}>
             <div className="character-info-button-text">Character Info</div>
           </button>
