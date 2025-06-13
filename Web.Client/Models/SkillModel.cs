302c5ea855683e7abaa54ac58c39f0d005dee15f
Håkon Anders Strømsodd <haakon8855@gmail.com>
2025-06-13 12:23:52 +0200

    Move skills content to json

---
+++ Web.Client/Models/SkillModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Web.Client.Models;
+
+public class SkillModel
+{
+    [JsonPropertyName("title")]
+    public required string Title { get; set; }
+    [JsonPropertyName("comment")]
+    public string? Comment { get; set; }
+}
---
+++ Web.Client/Models/SkillsModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Web.Client.Models;
+
+public class SkillsModel
+{
+    [JsonPropertyName("majorSkills")]
+    public required List<SkillModel> MajorSkills { get; set; }
+    [JsonPropertyName("minorSkills")]
+    public required List<SkillModel> MinorSkills { get; set; }
+}
